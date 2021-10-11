ec1c470076dddb57988451839a7a8d25059b4e53
Sergey Zeygerman <Sergey01@users.noreply.github.com>
2021-10-10 21:51:26 -0400

    Added documentation comment to isProxy.js

--- isProxy.js
+++ isProxy.js
@@ -1,3 +1,11 @@
+// Monkey patch for the native Javascript Proxy constructor function. This adds
+// a __isProxy property to any created Javascript proxies which returns true when
+// accessed. This does not modify the original target object nor is there any
+// other way to detect if a Proxy is being used (unless you want to go with things
+// like the postMessage hack but this is cleaner).
+//
+// The native Proxy function can be called via "nativeProxy".
+
 const nativeProxy = Proxy;
 Proxy = function(target, handler) {
   
